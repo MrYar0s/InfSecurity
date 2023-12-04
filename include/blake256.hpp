3f8f04cca276a76b92e10e3101f4dba7bad3e145
MrYar0s <molokan.yaroslavik@gmail.com>
2023-12-05 00:51:10 +0300

    Add Blake256

---
+++ include/blake256.hpp
@@ -0,0 +1,72 @@
+#ifndef BLAKE256_HPP
+#define BLAKE256_HPP
+
+#include <cstdint>
+#include <string>
+#include <array>
+
+#define U8TO32_BIG(p) \
+    (((uint32_t)((p)[0]) << 24) | ((uint32_t)((p)[1]) << 16) | ((uint32_t)((p)[2]) << 8) | ((uint32_t)((p)[3])))
+
+#define U32TO8_BIG(p, v)           \
+    (p)[0] = (uint8_t)((v) >> 24); \
+    (p)[1] = (uint8_t)((v) >> 16); \
+    (p)[2] = (uint8_t)((v) >> 8);  \
+    (p)[3] = (uint8_t)((v));
+
+#define U8TO64_BIG(p) (((uint64_t)U8TO32_BIG(p) << 32) | (uint64_t)U8TO32_BIG((p) + 4))
+
+#define U64TO8_BIG(p, v)                    \
+    U32TO8_BIG((p), (uint32_t)((v) >> 32)); \
+    U32TO8_BIG((p) + 4, (uint32_t)((v)));
+
+template <typename Type, uint8_t N>
+Type rotate(Type val)
+{
+    return (val << (32 - N)) | (val >> N);
+}
+
+class BLAKE256 final {
+public:
+    BLAKE256();
+
+    void reset();
+    void compress(const uint8_t *block);
+    void update(const uint8_t *in, int inlen);
+    void final(uint8_t *out);
+    void getHash(uint8_t *out, const uint8_t *in, uint64_t inlen);
+
+private:
+    std::array<uint32_t, 8> h_;
+    std::array<uint32_t, 4> s_;
+    std::array<uint32_t, 2> t_;
+    uint8_t m_[64];
+
+    bool nullt_;
+    int buflen_;
+
+    inline void round(uint32_t v[16], uint32_t m[16], uint8_t a, uint8_t b, uint8_t c, uint8_t d, uint8_t e,
+                      uint32_t i);
+
+    const uint8_t sigma[10][16] = {
+        {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}, {14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3},
+        {11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4}, {7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8},
+        {9, 0, 5, 7, 2, 4, 10, 15, 14, 1, 11, 12, 6, 8, 3, 13}, {2, 12, 6, 10, 0, 11, 8, 3, 4, 13, 7, 5, 15, 14, 1, 9},
+        {12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11}, {13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10},
+        {6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5}, {10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0}};
+
+    const uint32_t u[16] = {0x243f6a88, 0x85a308d3, 0x13198a2e, 0x03707344, 0xa4093822, 0x299f31d0,
+                            0x082efa98, 0xec4e6c89, 0x452821e6, 0x38d01377, 0xbe5466cf, 0x34e90c6c,
+                            0xc0ac29b7, 0xc97c50dd, 0x3f84d5b5, 0xb5470917};
+
+    uint32_t ivals[8] = {0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A,
+                         0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19};
+
+    const uint8_t padding[129] = {0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                  0,    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                  0,    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                  0,    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                  0,    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+};
+
+#endif  // BLAKE256_HPP
---
+++ src/blake256.cpp
@@ -0,0 +1,208 @@
+#include "blake256.hpp"
+#include <array>
+#include <cstdint>
+#include <cstring>
+#include <iostream>
+
+BLAKE256::BLAKE256()
+{
+    reset();
+}
+
+void BLAKE256::reset()
+{
+    for (uint32_t i = 0; i < 8; i++) {
+        h_[i] = ivals[i];
+    }
+    t_[0] = t_[1] = nullt_ = 0;
+    s_[0] = s_[1] = s_[2] = s_[3] = 0;
+    for (uint32_t i = 0; i < 64; i++) {
+        m_[i] = 0;
+    }
+}
+
+void BLAKE256::round(uint32_t v[16], uint32_t m[16], uint8_t a, uint8_t b, uint8_t c, uint8_t d, uint8_t e, uint32_t i)
+{
+    v[a] += (m[sigma[i % 10][e]] ^ u[sigma[i % 10][e + 1]]) + v[b];
+    v[d] = rotate<uint32_t, 16>(v[d] ^ v[a]);
+    v[c] += v[d];
+    v[b] = rotate<uint32_t, 12>(v[b] ^ v[c]);
+    v[a] += (m[sigma[i % 10][e + 1]] ^ u[sigma[i % 10][e]]) + v[b];
+    v[d] = rotate<uint32_t, 8>(v[d] ^ v[a]);
+    v[c] += v[d];
+    v[b] = rotate<uint32_t, 7>(v[b] ^ v[c]);
+}
+
+void BLAKE256::compress(const uint8_t *block)
+{
+    uint32_t v[16], m[16];
+
+    for (uint32_t i = 0; i < 16; ++i) {
+        m[i] = U8TO32_BIG(block + i * 4);
+    }
+
+    for (uint32_t i = 0; i < 8; ++i) {
+        v[i] = h_[i];
+    }
+
+    v[8] = s_[0] ^ u[0];
+    v[9] = s_[1] ^ u[1];
+    v[10] = s_[2] ^ u[2];
+    v[11] = s_[3] ^ u[3];
+    v[12] = u[4];
+    v[13] = u[5];
+    v[14] = u[6];
+    v[15] = u[7];
+
+    if (!nullt_) {
+        v[12] ^= t_[0];
+        v[13] ^= t_[0];
+        v[14] ^= t_[1];
+        v[15] ^= t_[1];
+    }
+
+    for (uint32_t i = 0; i < 14; ++i) {
+        round(v, m, 0, 4, 8, 12, 0, i);
+        round(v, m, 1, 5, 9, 13, 2, i);
+        round(v, m, 2, 6, 10, 14, 4, i);
+        round(v, m, 3, 7, 11, 15, 6, i);
+
+        round(v, m, 0, 5, 10, 15, 8, i);
+        round(v, m, 1, 6, 11, 12, 10, i);
+        round(v, m, 2, 7, 8, 13, 12, i);
+        round(v, m, 3, 4, 9, 14, 14, i);
+    }
+
+    for (uint32_t i = 0; i < 16; ++i) {
+        h_[i % 8] ^= v[i];
+    }
+
+    for (uint32_t i = 0; i < 8; ++i) {
+        h_[i] ^= s_[i % 4];
+    }
+}
+
+void BLAKE256::update(const uint8_t *in, int inlen)
+{
+    int left = buflen_;
+    int to_fill = 64 - left;
+
+    if (left && (inlen >= to_fill)) {
+        memcpy(m_ + left, in, to_fill);
+        t_[0] += 512;
+
+        if (t_[0] == 0) {
+            t_[1]++;
+        }
+
+        compress(m_);
+        in += to_fill;
+        inlen -= to_fill;
+        left = 0;
+    }
+    while (inlen >= 64) {
+        t_[0] += 512;
+
+        if (t_[0] == 0) {
+            t_[1]++;
+        }
+
+        compress(in);
+        in += 64;
+        inlen -= 64;
+    }
+
+    if (inlen > 0) {
+        memcpy(m_ + left, in, inlen);
+        buflen_ = left + inlen;
+    } else {
+        buflen_ = 0;
+    }
+}
+
+void BLAKE256::final(uint8_t *out)
+{
+    uint8_t msglen[8], zo = 0x01, oo = 0x81;
+    int lo = t_[0] + (buflen_ << 3), hi = t_[1];
+
+    if (lo < (buflen_ << 3)) {
+        hi++;
+    }
+
+    U32TO8_BIG(msglen + 0, hi);
+    U32TO8_BIG(msglen + 4, lo);
+
+    if (buflen_ == 55) {
+        t_[0] -= 8;
+        update(&oo, 1);
+    } else {
+        if (buflen_ < 55) {
+            if (!buflen_) {
+                nullt_ = 1;
+            }
+
+            t_[0] -= 440 - (buflen_ << 3);
+            update(padding, 55 - buflen_);
+        } else {
+            t_[0] -= 512 - (buflen_ << 3);
+            update(padding, 64 - buflen_);
+            t_[0] -= 440;
+            update(padding + 1, 55);
+            nullt_ = 1;
+        }
+
+        update(&zo, 1);
+        t_[0] -= 8;
+    }
+
+    t_[0] -= 64;
+    update(msglen, 8);
+    U32TO8_BIG(out + 0, h_[0]);
+    U32TO8_BIG(out + 4, h_[1]);
+    U32TO8_BIG(out + 8, h_[2]);
+    U32TO8_BIG(out + 12, h_[3]);
+    U32TO8_BIG(out + 16, h_[4]);
+    U32TO8_BIG(out + 20, h_[5]);
+    U32TO8_BIG(out + 24, h_[6]);
+    U32TO8_BIG(out + 28, h_[7]);
+}
+
+void BLAKE256::getHash(uint8_t *out, const uint8_t *in, uint64_t inlen)
+{
+    update(in, inlen);
+    final(out);
+}
+
+void blake256_test()
+{
+    int i, v;
+    uint8_t in[72], out[32];
+    uint8_t test1[] = {0x0c, 0xe8, 0xd4, 0xef, 0x4d, 0xd7, 0xcd, 0x8d, 0x62, 0xdf, 0xde, 0xd9, 0xd4, 0xed, 0xb0, 0xa7,
+                       0x74, 0xae, 0x6a, 0x41, 0x92, 0x9a, 0x74, 0xda, 0x23, 0x10, 0x9e, 0x8f, 0x11, 0x13, 0x9c, 0x87};
+    uint8_t test2[] = {0xd4, 0x19, 0xba, 0xd3, 0x2d, 0x50, 0x4f, 0xb7, 0xd4, 0x4d, 0x46, 0x0c, 0x42, 0xc5, 0x59, 0x3f,
+                       0xe5, 0x44, 0xfa, 0x4c, 0x13, 0x5d, 0xec, 0x31, 0xe2, 0x1b, 0xd9, 0xab, 0xdc, 0xc2, 0x2d, 0x41};
+    memset(in, 0, 72);
+    BLAKE256 blake;
+    blake.getHash(out, in, 1);
+    v = 0;
+
+    for (i = 0; i < 32; ++i) {
+        if (out[i] != test1[i])
+            v = 1;
+    }
+
+    if (v)
+        printf("test 1 error\n");
+
+    blake.reset();
+    blake.getHash(out, in, 72);
+    v = 0;
+
+    for (i = 0; i < 32; ++i) {
+        if (out[i] != test2[i])
+            v = 1;
+    }
+
+    if (v)
+        printf("test 2 error\n");
+}
